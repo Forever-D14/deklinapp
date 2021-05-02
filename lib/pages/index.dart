d1fb36909ddabde755e7baf5f2e07d7c4a8eab08
FOREVER <59987611+Forever-D14@users.noreply.github.com>
2021-05-02 17:31:30 -0500

    Cambio de Botones e Imagenes
    Se añadieron las imagenes originales por que daban error de las anteriores

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 //este será el menu de la app
@@ -23,7 +24,30 @@ class MenuPage extends StatelessWidget {
       body: Center(
         child: Column(
           children: <Widget>[
-            
+            Padding(
+              padding: EdgeInsets.only(left: 0.0, right: 0.0,top:50),
+              child: RaisedButton(
+                textColor: Colors.amber,
+                color: Colors.white70,
+                child: Column(
+                 children: [
+                   Container(child: Image.asset("assets/medal.png",height: 50,width: 50),padding: EdgeInsets.only(top:10, bottom: 20),),
+                   Container(child: Text("Principiante"),padding: EdgeInsets.only(left: 10,right: 10,bottom: 10),),
+                   Container(child: Align( alignment: Alignment.center,child: Text("Obten un pequeño ",)),padding: EdgeInsets.only(left: 10,right: 10)),
+                   Container(child: Align( alignment: Alignment.center,child: Text("tutorial teorico de la ",)),padding: EdgeInsets.only(left: 10,right: 10)),
+                   Container(child: Align( alignment: Alignment.center,child: Text("declinacion",)),padding: EdgeInsets.only(left: 10,right: 10)),
+
+
+                 ], 
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+              ),
+            )
            //aqui van los botones
 
             
